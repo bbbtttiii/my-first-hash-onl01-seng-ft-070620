8c74de42f5c771671ea115e8e7307a3aa72af99c
Ben Iburg <ben.iburg@gmail.com>
2020-07-09 14:12:33 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,25 +13,18 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
   shipping_manifest["oil paintings"]
-
 end
+
 
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
+                  "muskets" => 2,
+                  "gun powder" => 4
                  }
-  # add 2 muskets to the shipping_manifest hash below
 
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+  shipping_manifest
 end
